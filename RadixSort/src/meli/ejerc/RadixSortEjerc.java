26820f6ef8a8f27de2076270a7c8d742c032bb2d
Angeles Cerdosino <angeles.cerdosi@mercadolibre.com>
2021-04-04 16:37:40 -0300

    update

--- RadixSort/src/meli/ejerc/RadixSortEjerc.java
+++ RadixSort/src/meli/ejerc/RadixSortEjerc.java
@@ -1,20 +1,78 @@
 package meli.ejerc;
 
-public class RadixSortEjerc
-{
-	public static void radixSort(int []arr)
-	{
-		// PROGRAMAR AQUI
-	}
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-	public static void main(String[] args)
-	{
-		int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
-		radixSort(arr);
-		
-		for(int i=0; i<arr.length;i++)
-		{
-			System.out.print(arr[i]+(i<arr.length-1?",":""));
-		}
-	}
+public class RadixSortEjerc {
+    public static void radixSort(int[] arr) {               //array de int pasarlo a String
+        String cadena[] = StringUtil.toStringArray(arr);    //variable string que es array y lo igual a la doc StringUtil y convertirla en StringArray(arr)
+        StringUtil.lNormalize(cadena,'0');               //a lnormalize de stringutil le pasamos por parametro el string y el caracter a iniciar
+        int maximo = StringUtil.maxLength(cadena);
+        int position = maximo - 1;
+
+        sort(position, cadena);
+        arr = StringUtil.toIntArray(cadena);
+    }
+
+    private static void sort(int position, String[] cadena) {
+        ArrayList<String> primero = new ArrayList<>();
+        ArrayList<String> L0 = new ArrayList<>();
+        ArrayList<String> L1 = new ArrayList<>();
+        ArrayList<String> L2 = new ArrayList<>();
+        ArrayList<String> L3 = new ArrayList<>();
+        ArrayList<String> L4 = new ArrayList<>();
+        ArrayList<String> L5 = new ArrayList<>();
+        ArrayList<String> L6 = new ArrayList<>();
+        ArrayList<String> L7 = new ArrayList<>();
+        ArrayList<String> L8 = new ArrayList<>();
+        ArrayList<String> L9 = new ArrayList<>();
+        Map<Character, ArrayList<String>> lista = new HashMap<Character, ArrayList<String>>();
+        lista.put('0', L0);
+        lista.put('1', L1);
+        lista.put('2', L2);
+        lista.put('3', L3);
+        lista.put('4', L4);
+        lista.put('5', L5);
+        lista.put('6', L6);
+        lista.put('7', L7);
+        lista.put('8', L8);
+        lista.put('9', L9);
+
+        for (int i = 0; i < cadena.length; i++) {
+            String s = cadena[i];
+            Character c = s.charAt(position);
+            lista.get(c).add(s);
+        }
+
+        for (Map.Entry<Character, ArrayList<String>> entry : lista.entrySet()) {
+            ArrayList<String> v = entry.getValue();
+            if (v.size() > 0)
+                primero.addAll(v);
+        }
+
+
+        cadena = primero.toArray(new String[0]);
+        position = position - 1;
+        if(position > 0)
+            sort(position, cadena);
+
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int arr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
+        radixSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i < arr.length - 1 ? "," : ""));
+        }
+    }
 }
--- RadixSort/src/meli/ejerc/StringUtil.java
+++ RadixSort/src/meli/ejerc/StringUtil.java
@@ -1,47 +1,72 @@
 package meli.ejerc;
 
-public class StringUtil
-{
-	// Retorna una cadena compuesta por n caracteres c
-	// Ejemplo: replicate('x',5) ==> 'xxxxx'
-	public static String replicate(char c, int n)
-	{
-		return "";
-	}
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-	// Retorna una cadena de longitud n, compuesta por s
-	// y precedida de tantos caracteres c como sea necesario
-	// para completar la longitud mencionada
-	// Ejemplo lpad("5",3,'0') ==> "005"
-	public static String lpad(String s, int n, char c)
-	{
-		return "";
-	}
+public class StringUtil {
+    //Retorna una cadena compuesta por n caracteres c. Ejemplo: replicate('x',5) ==> 'xxxxx'
+    public static String replicate(char c, int n) {
+        String l = "";                      //declaro variable string
+        for (int i = 0; i < n; i++) {       //recorro el parametro n
+            l = l + c;                      //indico la variable declarada y la igualo sumando el caracter c, lo que cuenta cuantas veces se repite el caracter
+        }
+        return l;                           //retorno la variable
+    }
 
-	// Retorna un String[] conteniendo los elementos de arr
-	// representados como cadenas de caracteres
-	public static String[] toStringArray(int arr[])
-	{
-		return null;
-	}
 
-	// Retorna un String[] conteniendo los elementos de arr
-	// representados como cadenas de caracteres
-	public static int[] toIntArray(String arr[])
-	{
-		return null;
-	}
+    //Retorna una cadena de longitud n, compuesta por s y precedida de tantos caracteres c como sea necesario para completar la longitud mencionada.
+    //Ejemplo lpad("5",3,'0') ==> "005"
+    public static String lpad(String s, int n, char c) {
+        int m = n - s.length();                             //declaro variable m y lo igualo a la diferencia con n, que lo que hace s.length es agregar espacios hasta que el string es del tamaño deseado
+        String letra = replicate(c, m);                     //declaro variable String letra le asigno el valor que de como resultado del metodo replicate con los param c y m
+        String total = s + letra;                           //declaro otra variable total, le asigno el valor como resultado de s + la variable letra
+        return total;                                       // retorno la variab total
+    }
 
-	// Retorna la longitud del elemento con mayor cantidad
-	// de caracteres del array arr
-	public static int maxLength(String arr[])
-	{
-		return 0;
-	}
+    // Retorna un String[] conteniendo los elementos de arr representados como cadenas de caracteres
+    public static String[] toStringArray(int arr[]) {
 
-	// Completa los elemento del arr agregando caracteres c
-	// a la izquierda, dejando a todos con la longitud del mayor
-	public static void lNormalize(String arr[], char c)
-	{
-	}
+        String[] listaString = new String[arr.length];  //declaro variable de array string como una nueva lista de array con el tamaño que viene de parametro
+        ArrayList<Integer> ArrayI = new ArrayList<>();   //declaro un ArrayList del tipo wrapper Integer
+        for (int i = 0; i < arr.length; i++) {         // recorro de acuerdo al tamaño del arr
+            listaString[i] = Integer.toString(arr[i]);  //entonces de la listaString en la posicion [i], le asigno el valor en int del arr de esa posicion i
+        }
+        return listaString;                             //devuelvo la lista string
+    }
+
+    // Retorna un String[] conteniendo los elementos de arr representados como cadenas de caracteres
+    public static int[] toIntArray(String arr[]) {
+
+        int[] listaInt = new int [arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            listaInt[i] = Integer.parseInt(arr[i]);
+        }
+        return listaInt;
+    }
+
+    // Retorna la longitud del elemento con mayor cantidad de caracteres del array arr
+    public static int maxLength(String arr[]) {
+
+        int max = 0;
+        for (String s : arr ) {
+            int num = s.length();
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    // Completa los elemento del arr agregando caracteres c a la izquierda, dejando a todos con la longitud del mayor
+    public static void lNormalize(String arr[], char c) {
+
+            int max = maxLength(arr);                   //declaro variable max y le asigno el valor del metodo anterior maxLength con el parametro que ya trae dentro
+            for (int i = 0; i < arr.length ; i++) {     //recorro las posiciones declarando mientras que i valga cero, si i es menor a la longitud del arr y aumento i
+                if(arr[i].length() < max){              //condiciono si arr en la posicion i de acuerdo a su tamaño si es menor a la bandera max, entonces
+                    String s = lpad(arr[i], max, c);    //declaro variable string y le asigno el valor del metodo lpad con el parametro del arr en la posicion i, mi variable max y caracter c
+                    arr[i] = s;                         //será el arr en la posicion i igual a la variable string s
+                }
+            }
+
+    }
 }
